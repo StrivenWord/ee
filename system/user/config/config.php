d075bfe1bb7f2ee46d68e3846c32a3e4ba1694ba
Paul Lee <paul_lee@zoho.com>
2022-10-08 12:17:16 -0700

    Initial commit

---
+++ system/user/config/config.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// ExpressionEngine Config Items
+// Find more configs and overrides at
+// https://docs.expressionengine.com/latest/general/system-configuration-overrides.html
+
+$config['app_version'] = '7.1.6';
+$config['encryption_key'] = '248d0a90ed7b5b45edebc10b8fc4b89925fa6ddd';
+$config['session_crypt_key'] = '2314f6a4fa3f69057670b196282375bb3e57fd04';
+$config['database'] = array(
+	'expressionengine' => array(
+		'hostname' => 'localhost',
+		'database' => 'exe',
+		'username' => 'usuario',
+		'password' => 'RgM(JWO@hsKT*bKN',
+		'dbprefix' => 'exp_',
+		'char_set' => 'utf8mb4',
+		'dbcollat' => 'utf8mb4_unicode_ci',
+		'port'     => ''
+	),
+);
+$config['show_ee_news'] = 'y';
+$config['share_analytics'] = 'y';
+
+// EOF
